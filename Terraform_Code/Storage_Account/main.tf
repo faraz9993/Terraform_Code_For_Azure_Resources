7dedf11c2a24b00dc089311683335d38c3424f33
Farajnazish Ansari <faraj.ansari@einfochips.com>
2024-12-04 12:18:45 +0530

    Adding Terraform Code for Storage Account

--- Terraform_Code/Storage_Account/main.tf
+++ Terraform_Code/Storage_Account/main.tf
@@ -1,62 +1,20 @@
 provider "azurerm" {
   features {}
-  subscription_id = "XXXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXX"
-  resource_provider_registrations = "none"
+  subscription_id = var.subscription_id
+  resource_provider_registrations = var.resource_provider_registrations
 }
-resource "azurerm_storage_account" "sa1deveicipimplemen8ad4" {
-    account_kind                       = var.account_kind
-    account_replication_type           = var.account_replication_type
-    account_tier                       = var.account_tier
-    allow_nested_items_to_be_public    = var.allow_nested_items_to_be_public
-    cross_tenant_replication_enabled   = var.cross_tenant_replication_enabled
-    default_to_oauth_authentication    = var.default_to_oauth_authentication
-    dns_endpoint_type                  = var.dns_endpoint_type
-    https_traffic_only_enabled         = var.https_traffic_only_enabled
-    infrastructure_encryption_enabled  = var.infrastructure_encryption_enabled
-    is_hns_enabled                     = var.is_hns_enabled
-    large_file_share_enabled           = var.large_file_share_enabled
-    local_user_enabled                 = var.local_user_enabled
-    location                           = var.location
-    min_tls_version                    = var.min_tls_version
-    name                               = var.name
-    nfsv3_enabled                      = var.nfsv3_enabled
-    public_network_access_enabled      = var.public_network_access_enabled
-    queue_encryption_key_type          = var.queue_encryption_key_type
-    resource_group_name                = var.resource_group_name
-    sftp_enabled                       = var.sftp_enabled
-    shared_access_key_enabled          = var.shared_access_key_enabled
-    table_encryption_key_type          = var.table_encryption_key_type
-    tags                               = {}
 
-    blob_properties {
-        change_feed_enabled           = var.change_feed_enabled
-        last_access_time_enabled      = var.last_access_time_enabled
-        versioning_enabled            = var.versioning_enabled
-    }
-
-    queue_properties {
-        hour_metrics {
-            enabled               = var.enabled
-            include_apis          = var.include_apis
-            retention_policy_days = 7
-            version               = "1.0"
-        }
-        logging {
-            delete                = var.delete
-            read                  = var.read
-            version               = "1.0"
-            write                 = var.write
-        }
-        minute_metrics {
-            enabled               = var.enabled
-            include_apis          = var.include_apis
-            version               = "1.0"
-        }
-    }
-
-    share_properties {
-        retention_policy {
-            days = 7
-        }
-    }
+resource "azurerm_storage_account" "devtestinstorageaccount" {
+  name                           = var.name
+  resource_group_name            = var.resource_group_name
+  location                       = var.location
+  account_kind                   = var.account_kind
+  account_replication_type       = var.account_replication_type
+  account_tier                   = var.account_tier
+  network_rules {
+    bypass                     = [
+      "AzureServices",
+    ]
+    default_action             = "Deny"
+  }
 }
--- Terraform_Code/Storage_Account/variables.tf
+++ Terraform_Code/Storage_Account/variables.tf
@@ -1,160 +1,40 @@
-variable "account_kind" {
+variable "subscription_id" {
+  description = "Azure Subscription ID"
   type        = string
-  default     = "Storage"
-}
-
-variable "account_replication_type" {
-  type        = string
-  default     = "LRS"
-}
-
-variable "account_tier" {
-  type        = string
-  default     = "Standard"
-}
-
-variable "dns_endpoint_type" {
-  type        = string
-  default     = "Standard"
-}
-
-variable "location" {
-  type        = string
-  default     = "southeastasia"
-}
-
-variable "min_tls_version" {
-  type        = string
-  default     = "TLS1_2"
 }
 
 variable "name" {
   type        = string
-  default     = "farazstorageaccount"
 }
 
-
-variable "queue_encryption_key_type" {
+variable "resource_provider_registrations" {
   type        = string
-  default     = "Service"
 }
 
 variable "resource_group_name" {
   type        = string
-  default     = "sa1_test_eic_FarajAnsari"
 }
 
-variable "table_encryption_key_type" {
+variable "location" {
   type        = string
-  default     = "Service"
 }
 
-
-variable "allow_nested_items_to_be_public" {
-  type        = bool
-  default     = false
+variable "sku_name" {
+  type        = string
 }
 
-variable "cross_tenant_replication_enabled" {
-  type        = bool
-  default     = false
+variable "bypass" {
+  type        = string
 }
 
-variable "default_to_oauth_authentication" {
-  type        = bool
-  default     = true
+variable "account_kind" {
+  type        = string
 }
 
-variable "https_traffic_only_enabled" {
-  type        = bool
-  default     = true
+variable "account_replication_type" {
+  type        = string
 }
 
-variable "infrastructure_encryption_enabled" {
-  type        = bool
-  default     = false
-}
-
-variable "is_hns_enabled" {
-  type        = bool
-  default     = false
-}
-
-variable "large_file_share_enabled" {
-  type        = bool
-  default     = false
-}
-
-variable "local_user_enabled" {
-  type        = bool
-  default     = true
-}
-
-variable "nfsv3_enabled" {
-  type        = bool
-  default     = false
-}
-
-variable "public_network_access_enabled" {
-  type        = bool
-  default     = true
-}
-
-variable "sftp_enabled" {
-  type        = bool
-  default     = false
-}
-
-variable "shared_access_key_enabled" {
-  type        = bool
-  default     = true
-}
-################
-#blob_properties
-################
-
-variable "change_feed_enabled" {
-  type        = bool
-  default     = false
-}
-
-variable "last_access_time_enabled" {
-  type        = bool
-  default     = false
-}
-
-variable "versioning_enabled" {
-  type        = bool
-  default     = false
-}
-#################
-#queue_properties
-#################
-
-variable "enabled" {
-  type        = bool
-  default     = true
-}
-
-variable "include_apis" {
-  type        = bool
-  default     = false
-}
-##########
-#logging
-#########
-
-variable "delete" {
-  type        = bool
-  default     = false
-}
-
-variable "read" {
-  type        = bool
-  default     = false
-}
-
-variable "write" {
-  type        = bool
-  default     = false
+variable "account_tier" {
+  type        = string
 }
